b9ead39bc21c7fea42b4497b66fd29ae909060e0
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2026-03-12 15:15:25 +0100

    Restriction Operators

--- LINQ_Project/Program.cs
+++ LINQ_Project/Program.cs
@@ -5,8 +5,27 @@ namespace LINQ_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ProductList[0]);
-            Console.WriteLine(CustomerList[0]);
+            //Console.WriteLine(ProductList[0]);
+            //Console.WriteLine(CustomerList[0]);
+
+            #region Restriction Operators
+            /// 1. Find all products that are out of stock.
+            var Result1 = ProductList.Where(P => P.UnitsInStock == 0);
+            foreach (var item in Result1)
+                Console.WriteLine(item);
+
+            /// 2.Find all products that are in stock and cost more than 3.00 per unit.
+            var Result2 = ProductList.Where(P => P.UnitsInStock > 0 && P.UnitPrice > 3.00m);
+            foreach (var item in Result2)
+                Console.WriteLine(item);
+
+            /// 3.Returns digits whose name is shorter than their value.
+            string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            var Result3 = Arr.Where((V, I) => V.Length < I);
+            foreach (var item in Result3)
+                Console.WriteLine(item);
+            #endregion
         }
     }
 }
