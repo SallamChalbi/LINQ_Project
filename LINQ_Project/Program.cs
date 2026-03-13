f53891df078e019963f1b1ba2a1952df300a9abf
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2026-03-13 15:56:44 +0100

    empty commit

--- LINQ_Project/Program.cs
+++ LINQ_Project/Program.cs
@@ -82,63 +82,65 @@ namespace LINQ_Project
             #endregion
 
             #region Transformation Operators
-            /// 1. Return a sequence of just the names of a list of products.
-            var Result1 = ProductList.Select(P => P.ProductName);
-            foreach (var Product in Result1)
-                Console.WriteLine(Product);
+            ///// 1. Return a sequence of just the names of a list of products.
+            //var Result1 = ProductList.Select(P => P.ProductName);
+            //foreach (var Product in Result1)
+            //    Console.WriteLine(Product);
 
-            /// 2. Produce a sequence of the uppercase and lowercase versions of each word in the original array (Anonymous Types).
-            string[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
-            var Result2 = words.Select(W => new
-            {
-                Uppercase = W.ToUpper(),
-                Lowercase = W.ToLower(),
-            });
-            foreach (var result in Result2)
-                Console.WriteLine(result);
+            ///// 2. Produce a sequence of the uppercase and lowercase versions of each word in the original array (Anonymous Types).
+            //string[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+            //var Result2 = words.Select(W => new
+            //{
+            //    Uppercase = W.ToUpper(),
+            //    Lowercase = W.ToLower(),
+            //});
+            //foreach (var result in Result2)
+            //    Console.WriteLine(result);
 
-            /// 3.Produce a sequence containing some properties of Products, including UnitPrice which is renamed to Price in the resulting type.
-            var Result3 = ProductList.Select(P => new
-            {
-                P.ProductID,
-                P.ProductName,
-                Price = P.UnitPrice
-            });
-            foreach (var Result in Result3)
-                Console.WriteLine(Result);
+            ///// 3.Produce a sequence containing some properties of Products, including UnitPrice which is renamed to Price in the resulting type.
+            //var Result3 = ProductList.Select(P => new
+            //{
+            //    P.ProductID,
+            //    P.ProductName,
+            //    Price = P.UnitPrice
+            //});
+            //foreach (var Result in Result3)
+            //    Console.WriteLine(Result);
 
-            /// 4.Determine if the value of ints in an array match their position in the array.
-            int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-            var Result4 = Arr.Select((V, I) => $"{V}:{V == I}");
-            foreach (var result in Result4)
-                Console.WriteLine(result);
+            ///// 4.Determine if the value of ints in an array match their position in the array.
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result4 = Arr.Select((V, I) => $"{V}:{V == I}");
+            //foreach (var result in Result4)
+            //    Console.WriteLine(result);
 
-            /// 5.Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
-            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
-            int[] numbersB = { 1, 3, 5, 7, 8 };
-            var Result5 = from a in numbersA
-                          from b in numbersB
-                          where a < b
-                          select $"{a} is less than {b}";
-            //// or
-            //var Result5 = numbersA.SelectMany(N => numbersB, (a, b) => new {a, b})
-            //                      .Where(Num => Num.a < Num.b)
-            //                      .Select(Num => $"{Num.a} is less than {Num.b}");
-            foreach (var result in Result5)
-                Console.WriteLine(result);
+            ///// 5.Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
+            //var Result5 = from a in numbersA
+            //              from b in numbersB
+            //              where a < b
+            //              select $"{a} is less than {b}";
+            ////// or
+            ////var Result5 = numbersA.SelectMany(N => numbersB, (a, b) => new {a, b})
+            ////                      .Where(Num => Num.a < Num.b)
+            ////                      .Select(Num => $"{Num.a} is less than {Num.b}");
+            //foreach (var result in Result5)
+            //    Console.WriteLine(result);
 
-            /// 6. Select all orders where the order total is less than 500.00.
-            var Result6 = CustomerList.SelectMany(C => C.Orders)
-                                     .Where(O => O.Total < 500.00m);
-            foreach (var result in Result6)
-                Console.WriteLine(result);
+            ///// 6. Select all orders where the order total is less than 500.00.
+            //var Result6 = CustomerList.SelectMany(C => C.Orders)
+            //                         .Where(O => O.Total < 500.00m);
+            //foreach (var result in Result6)
+            //    Console.WriteLine(result);
 
-            /// 7. Select all orders where the order was made in 1998 or later.
-            var Result7 = CustomerList.SelectMany(C => C.Orders)
-                                      .Where(O => O.OrderDate.Year >= 1998);
-            foreach(var result in Result7) 
-                Console.WriteLine(result);
+            ///// 7. Select all orders where the order was made in 1998 or later.
+            //var Result7 = CustomerList.SelectMany(C => C.Orders)
+            //                          .Where(O => O.OrderDate.Year >= 1998);
+            //foreach(var result in Result7) 
+            //    Console.WriteLine(result);
             #endregion
+
+            
         }
     }
 }
